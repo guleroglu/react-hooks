480411a525298fce254b3cf53db4ee7f5f238fe3
Mustafa Guleroglu <must60yyy@gmail.com>
2024-03-28 21:54:53 +0300

    added use-full-screen hook

---
+++ src/hooks/use-full-screen.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useFullScreen() {
+  const [fullScreen, setFullScreen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", () => {
+      if (document.fullscreenElement) {
+        setFullScreen(true);
+      } else {
+        setFullScreen(false);
+      }
+    });
+  }, []);
+
+  const toggle = () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen().catch(() => alert("Full screen hatasi"));
+    } else {
+      (ref.current || document.documentElement)
+        .requestFullscreen()
+        .catch(() => alert("Full screen hatasi"));
+    }
+  };
+
+  return {
+    ref,
+    toggle,
+    fullScreen,
+  };
+}
