f94eaaf3a6ca68edce5ef0f5e3f370ae69e90c81
Mustafa Guleroglu <must60yyy@gmail.com>
2024-03-28 21:18:08 +0300

    added use-focus-within

---
+++ src/hooks/use-focus-within.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useFocusWithin() {
+  const [focused, setFocused] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      const focusInHandle = () => {
+        setFocused(true);
+      };
+
+      const focusOutHandle = (e) => {
+        if (!ref.current.contains(e.relatedTarget)) {
+          setFocused(false);
+        }
+      };
+
+      ref.current.addEventListener("focusin", focusInHandle);
+      ref.current.addEventListener("focusout", focusOutHandle);
+    }
+  }, [ref]);
+
+  return {
+    focused,
+    ref,
+  };
+}
